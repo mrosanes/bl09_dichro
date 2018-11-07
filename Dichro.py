f1ab04f746c6413fd30af5bf53192249b89ccc40
cfalcon <cfalcon@cells.es>
2018-11-07 16:37:16 +0100

    Collect scans in snake mode for JJ positions

--- Dichro.py
+++ Dichro.py
@@ -1,25 +1,22 @@
 # TEST SCRIPT FOR SEEING IF IT IS POSSIBLE TO AUTOMATE A TOMO with 2 polarizations.
-
 import numpy as np
 
 # Filenames
-file_name = 'dichro.txt'
-file_name_collect = '{0}_collect.{1}'.format(*file_name.rsplit('.', 1))
-file_name_ff = '{0}_ff.{1}'.format(*file_name.rsplit('.', 1))
+file_name = 'F40N80F80BRot.txt'
+
 # NUM OF TOMOS
 # num_of_tomos = 1
 
-# TODO Why this naming convention? it does not follow manytomos naming
 # Tomo parameters;
 # naming convention date_sampleName_JJoffset_angle_number.xrm
 # FF naming convention date_sampleName_JJoffset_FF_number.xrm
 
-date = '20180531'
-sample_name_1 = 'name'
+date = '20180603'
+sample_name_1 = 'F40N80F80BRot'
 
-binning = 1  # set your binning, unit: field of view
-exptime1 = 8  # set your exposure time, unit: second
-exptime2 = 8
+binning = 1  #set your binning, unit: field of view
+exptime1 = 20  #set your exposure time, unit: second
+exptime2 = 12
 exptime3 = 10
 exptimeFF = 3
 
@@ -27,39 +24,55 @@ exptimeFF = 3
 E = 707.4
 
 # define sample & FF positions
-X = 22
-Y = 435.3
-Z = -600
-FF_Z = -1000
-FF2_Z = Z  # to come back to initial sample positions
+X = -89.4
+Y = -227.6
+Z = -639.1
+FF_Z = -1100
+FF2_Z = Z #to come back to initial sample positions
 FF_X = 1700
-FF2_X = X  # to come back to initiacl sample positions
-# FF_Y = 100
+FF2_X = X #to come back to initiacl sample positions
+#FF_Y = 100
 
 # Define JJ_up & JJ_down
-JJU_1 = 0.9  # UP
-JJD_1 = -2.1  # UP
-JJU_2 = -3.2  # DOWN
-JJD_2 = -6.2  # DOWN
-JJU_3 = 10  # to open after macro has finished
-JJD_3 = -10  # to open after macro has finished
+JJU_1 = 0.9 #UP
+JJD_1 = -2.2 #UP
+JJU_2 = -3.1 #DOWN
+JJD_2 = -6.2 #DOWN
+JJU_3 = 10 #to open after macro has finished
+JJD_3 = -10 #to open after macro has finished
 JJ_offset_1 = (JJU_1 + JJD_1) / 2.0
 JJ_offset_2 = (JJU_2 + JJD_2) / 2.0
 
 T0 = 0
-Tini = -55
-T_0 = -54
-T_1 = -40
-T_2 = 40
-T_3 = 54
-T_step = 2.0
+Tini = -55.5
+T_0 = -55
+T_1 = -44
+T_2 = -24
+T_3 = 24
+T_4 = 44
+T_5 = 55
+
+T_step1 = 1.0
+T_step2 = 2.0
 FF_T = -45
 FF2_T = 0
 
-repetitions = 10
-repetitions_FF = 10
+# Zoneplate Values
+ZPz_1 = -11309.1
+ZPz_2 = -11307.6
+
+#repetitions = 10
+repetitions_FF = 50
+wait_time_JJ = 80
 
 
+###############################################################################
+## Aux Functions DO NOT MODIFY
+###############################################################################
+file_name_collect = '{0}_collect.{1}'.format(*file_name.rsplit('.', 1))
+file_name_ff = '{0}_ff.{1}'.format(*file_name.rsplit('.', 1))
+
+# Function: Move select and target
 def set_select_and_target(file, select, target):
     # Move select
     file.write('moveto prx %6.2f\n' % select)
@@ -67,66 +80,73 @@ def set_select_and_target(file, select, target):
     file.write('moveto pry %6.2f\n' % target)
 
 
-with open(file_name_collect, 'w') as f:
+# Function: Move jjs to position 1
+def move_to_jj_1(file):
+    file.write('moveto phy %6.2f\n' %JJD_1)
+    file.write('moveto phx %6.2f\n' %JJU_1)
+    file.write('moveto ZPz %6.2f\n' %ZPz_1)
+    file.write('wait %d\n' % wait_time_JJ)
+
+# Function: Move jjs to position 2
+def move_to_jj_2(file):
+    file.write('moveto phy %6.2f\n' %JJD_2)
+    file.write('moveto phx %6.2f\n' %JJU_2)
+    file.write('moveto ZPz %6.2f\n' %ZPz_2)
+    file.write('wait %d\n' % wait_time_JJ)
+
+# Function: Collection for a single angle
+def collect_many_repetitions(file, T, counter, repetitions):
+    file.write('moveto T %6.2f\n' %T)
+    if counter % 2 == 0:
+        for j in range(repetitions):
+            file.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_1, T, j))
+        move_to_jj_2(file)
+        for j in range(repetitions):
+            file.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_2, T, j))
+    else:
+        for j in range(repetitions):
+            file.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_2, T, j))
+        move_to_jj_1(file)
+        for j in range(repetitions):
+            file.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_1, T, j))
+
+# Function: Collection for a full angular region
+def angular_region_collection(file, repetitions, exptime, T_initial, T_final, T_step):
+    counter = 0
+    move_to_jj_1(file)
+    file.write('setexp ' + str(exptime) + '\n')
+    for T in np.arange(T_initial, T_final, T_step):
+        collect_many_repetitions(file, T, counter, repetitions)
+        counter += 1
+
+###############################################################################
+
+
+
+# Define regions to collect
+with open(file_name_collect, 'w') as collect_file:
     # Move to Energy: important for preprocessing
-    f.write('moveto energy %6.2f\n' % E)
-
+    collect_file.write('moveto energy %6.2f\n' % E)
     #### Confirm Sample Position ####
-    f.write('moveto X %6.2f\n' % X)
-    f.write('moveto Y %6.2f\n' % Y)
-    f.write('moveto Z %6.2f\n' % Z)
-
+    collect_file.write('moveto X %6.2f\n' % X)
+    collect_file.write('moveto Y %6.2f\n' % Y)
+    collect_file.write('moveto Z %6.2f\n' % Z)
     #### JU_1, JD_1 ###
+    collect_file.write('setbinning ' + str(binning) + '\n')
+    collect_file.write('moveto T %6.2f\n' % Tini)
+    # You can define new regions below
+    angular_region_collection(collect_file, 25, exptime1, T_0, T_1 + T_step1, T_step1)
+    angular_region_collection(collect_file, 20, exptime2, T_1 + T_step1, T_2 + T_step1, T_step1)
+    angular_region_collection(collect_file, 18, exptime3, T_2 + T_step2, T_3 + T_step2, T_step2)
+    angular_region_collection(collect_file, 20, exptime2, T_3 + T_step1, T_4 + T_step1, T_step1)
+    angular_region_collection(collect_file, 25, exptime1, T_4 + T_step1, T_5 + T_step1, T_step1)
 
-    f.write('setbinning ' + str(binning) + '\n')
 
-    set_select_and_target(f, 0, 0)
-    f.write('moveto T %6.2f\n' % Tini)
 
-    for T in np.arange(T_0, T_1 + T_step, T_step):
-        f.write('setexp ' + str(exptime1) + '\n')
-        f.write('moveto T %6.2f\n' % T)
-        f.write('moveto phy %6.2f\n' % JJU_1)
-        f.write('moveto phx %6.2f\n' % JJD_1)
-        f.write('wait 80\n')
-        for j in range(repetitions):
-            f.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_1, T, j))
-        f.write('moveto phy %6.2f\n' % JJU_2)
-        f.write('moveto phx %6.2f\n' % JJD_2)
-        f.write('wait 80\n')
-        for j in range(repetitions):
-            f.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_2, T, j))
-        set_select_and_target(f, 1, T)
 
-    for T in np.arange(T_1 + T_step, T_2 + T_step, T_step):
-        f.write('setexp ' + str(exptime2) + '\n')
-        f.write('moveto T %6.2f\n' % T)
-        f.write('moveto phy %6.2f\n' % JJU_1)
-        f.write('moveto phx %6.2f\n' % JJD_1)
-        f.write('wait 80\n')
-        for j in range(repetitions):
-            f.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_1, T, j))
-        f.write('moveto phy %6.2f\n' % JJU_2)
-        f.write('moveto phx %6.2f\n' % JJD_2)
-        f.write('wait 80\n')
-        for j in range(repetitions):
-            f.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_2, T, j))
-        set_select_and_target(f, 1, T)
-
-    for T in np.arange(T_2 + T_step, T_3 + T_step, T_step):
-        f.write('setexp ' + str(exptime3) + '\n')
-        f.write('moveto T %6.2f\n' % T)
-        f.write('moveto phy %6.2f\n' % JJU_1)
-        f.write('moveto phx %6.2f\n' % JJD_1)
-        f.write('wait 80\n')
-        for j in range(repetitions):
-            f.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_1, T, j))
-        f.write('moveto phy %6.2f\n' % JJU_2)
-        f.write('moveto phx %6.2f\n' % JJD_2)
-        f.write('wait 80\n')
-        for j in range(repetitions):
-            f.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_2, T, j))
-        set_select_and_target(f, 1, T)
+###############################################################################
+## Create microscope collect files DO NOT MODIFY
+###############################################################################
 
 # Copy file_name_collect contents in file_name
 with open(file_name, 'w') as outfile:
@@ -135,27 +155,23 @@ with open(file_name, 'w') as outfile:
 
 # FF Acquisition
 with open(file_name_ff, 'w') as ff_file:
-    # TODO is it need?
-    ff_file.write('moveto T %6.2f\n' % T0)
     ff_file.write('moveto T %6.2f\n' % FF_T)
     ff_file.write('moveto Z %6.2f\n' % FF_Z)
     ff_file.write('moveto X %6.2f\n' % FF_X)
     ff_file.write('setexp ' + str(exptimeFF) + '\n')
-    for j in range(repetitions_FF):
-        ff_file.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_2, "FF", j))
-
-    ff_file.write('moveto phy %6.2f\n' % JJU_1)
-    ff_file.write('moveto phx %6.2f\n' % JJD_1)
-    ff_file.write('wait 80\n')
+    move_to_jj_1(ff_file)
     for j in range(repetitions_FF):
         ff_file.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_1, "FF", j))
+
+    move_to_jj_2(ff_file)
+    for j in range(repetitions_FF):
+        ff_file.write('collect {0}_{1}_{2}_{3}_{4}.xrm\n'.format(date, sample_name_1, JJ_offset_2, "FF", j))
 
 # Copy file_name_collect contents in file_name
 with open(file_name, 'a') as outfile:
     with open(file_name_ff) as infile:
         outfile.write(infile.read())
 
-# TODO is it need?
 with open(file_name_collect, 'a') as collect_file, open(file_name, 'a') as outfile:
     collect_file.write('moveto X %6.2f\n' % FF2_X)
     outfile.write('moveto X %6.2f\n' % FF2_X)
@@ -167,3 +183,4 @@ with open(file_name_collect, 'a') as collect_file, open(file_name, 'a') as outfi
     outfile.write('moveto phy %6.2f\n' % JJU_3)
     collect_file.write('moveto phx %6.2f\n' % JJD_3)
     outfile.write('moveto phx %6.2f\n' % JJD_3)
+###############################################################################
